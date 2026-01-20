b1f3839f523a8c2b307d0625c031b0c6956a2c09
juanjore0 <juan.arango7@utp.edu.co>
2026-01-19 20:02:23 -0500

    Añadir punto de entrada para ejecutar el servidor Uvicorn

--- backend/main.py
+++ backend/main.py
@@ -104,3 +104,7 @@ def get_current_user(authorization: str = Header(None), db: Session = Depends(ge
         "full_name": user.full_name,
         "email": user.email
     }
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
